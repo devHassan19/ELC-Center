2c3a304189372576534faaaa3d4c2c6fed3a1928
devhassan19 <devhassan19@email.com>
2024-11-19 21:06:41 +0300

    Complete Auth

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: [3, 'Name must to more 3 letter']
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: Number,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    userType: {
+      type: String,
+      required: true,
+      enum: ['admin', 'teacher', 'student']
+    }
+  },
+  {
+    timestamps: true //means created At and update At
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ server.js
@@ -0,0 +1,59 @@
+//--------------Const -----------------//
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+const session = require('express-session')
+const isSignIn = require('./middleware/is-signed-in.js')
+const passUserToView = require('./middleware/pass-user-to-view.js')
+
+const port = process.env.PORT ? process.env.PORT : '3000'
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
+app.use(passUserToView)
+
+app.use((req, res, next) => {
+  if (req.session.message) {
+    console.log('req.session.message', req.session.message)
+    res.locals.message = req.session.message
+    req.session.message = null
+  } else {
+    res.locals.message = null
+  }
+  next()
+})
+
+// Require Controller
+const authController = require('./controllers/auth')
+
+// Use Controllers
+app.use('/auth', authController)
+
+app.get('/', async (req, res) => {
+  res.render('index.ejs')
+})
+app.get('/vip-lounge', isSignIn, (req, res) => {
+  res.send(`Welcome to the party ${req.session.user.username}`)
+})
+
+app.listen(port, () => {
+  console.log(`The express app is ready on part ${port}`)
+})
