cfb52a608c62e380874043e749e52a9462c02c33
devhassan19 <devhassan19@email.com>
2024-11-26 20:28:22 +0300

    complete student section

--- controllers/student.js
+++ controllers/student.js
@@ -3,35 +3,55 @@ const router = express.Router()
 const Student = require('..//models/user')
 const Class = require('../models/class')
 
-router.get('/admin', async (req, res) => {
+// router.get('/', async (req, res) => {
+//   try {
+//     const populatedClass = await Class.find({})
+//       .populate('teacher')
+//       .populate('subject')
+//     res.render('student/index.ejs', { Class: populatedClass })
+//   } catch (error) {
+//     console.log(error)
+//     res.redirect('/student')
+//   }
+// })
+
+router.get('/', async (req, res) => {
   try {
-    const populatedClass = await Class.find({})
-    res.render('student/index.ejs', { Class: populatedClass })
+    const mySubject = await Class.find({
+      student: req.session.user._id
+    })
+      .populate('owner')
+      .populate('teacher')
+      .populate('subject')
+    res.render('student/index.ejs', { mySubject })
   } catch (error) {
     console.log(error)
-    res.redirect('/admin')
+    res.redirect('/student')
   }
 })
 
 router.get('/new', async (req, res) => {
   try {
     const populatedClass = await Class.find({})
+      .populate('teacher')
+      .populate('subject')
     res.render('student/new.ejs', { Class: populatedClass })
   } catch (error) {
     console.log(error)
-    res.redirect('/admin')
+    res.redirect('/student')
   }
 })
 
 router.post('/:populatedClass/join-by/:userId', async (req, res) => {
   try {
+    req.body.owner = req.session.user._id
     await Class.findByIdAndUpdate(req.params.populatedClass, {
       $push: { student: req.params.userId }
     })
     res.redirect('/student')
   } catch (error) {
     console.log(error)
-    res.redirect('/admin')
+    res.redirect('/student')
   }
 })
 
